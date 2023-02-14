6deb3d0fe0a7be6fc14229004783a97708de6beb
mohat.shil <mohat_shil@epam.com>
2023-02-14 16:07:40 +0100

    Revert "Module_one Unit testing"
    This reverts commit 643fc92bd17465625a74776f579d990b8882875f.

--- module-2/tests/getEvenNumbersFromArray.test.js
+++ module-2/tests/getEvenNumbersFromArray.test.js
@@ -13,12 +13,4 @@ describe('getEvenNumbersFromArray', function () {
     const arrayOfRandomNumbers = [2, 7, 12, 17, 1, 55, 32, 10];
     expect(validator.getEvenNumbersFromArray(arrayOfRandomNumbers)).to.be.eql([2, 12, 32, 10]);
   });
-
-  it('should return error for array of number',()=>{
-    const arrayValue = [10,'20',30];
-    expect(()=>{
-      validator.getEvenNumbersFromArray(arrayValue);
-    }).to.throw('[10,20,30] is not an array of "Numbers"')
-  });
-
 });
