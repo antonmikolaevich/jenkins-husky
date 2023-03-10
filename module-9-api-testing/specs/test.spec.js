eefd7cbc754dc06f5f9c6b7f66289cb5ba6300ce
mohat.shil <mohat_shil@epam.com>
2023-03-10 18:08:46 +0100

    negative cases updated as well.

--- module-9-api-testing/specs/test.spec.js
+++ module-9-api-testing/specs/test.spec.js
@@ -39,26 +39,52 @@ describe('POSITIVE API TEST SUITE', ()=>{
 
 describe('NEGATIVE API TEST SUITE', ()=>{
 
-    it('GET post/1', async()=>{
+    it('GET post/1 invalid status', async()=>{
         const response = await sendRequest('/posts/1',testData,'Get2')
         expect(response.status).to.equal(400)
     })
 
-    it('POST post', async ()=>{
+    it('GET post/1 invalid request', async()=>{
+        const response = await sendRequest('/posts/1')
+        expect(response.data.title).to.not.equal(2)
+        expect(response.data.userID).to.not.equal(1001)
+    })
+
+    it('POST post invalid status', async ()=>{
 
         const response = await sendRequest('/posts',testData,"pos")
         expect(response.status).to.equal(400)
 
     })
 
-    it('UPDATE post', async()=>{
+    it('POST post invalid request', async ()=>{
+
+        const response = await sendRequest('/posts',testData,"post")
+        expect(response.data.name).to.not.equal("Jhone")
+        expect(response.data.userID).to.not.equal(1001)
+
+    })
+
+    it('UPDATE post invalid status', async()=>{
         const response = await sendRequest('/posts/1',testData,'PU');
         expect(response.status).to.equal(400)
     })
 
-    it('DELETE post', async()=>{
+    it('UPDATE post invalid request', async()=>{
+        const response = await sendRequest('/posts/1',testData,'PUT');
+        expect(response.data.name).to.not.equal("Marlo")
+        expect(response.data.userID).to.not.equal(1011)
+    })
+
+    it('DELETE post invalid status', async()=>{
         const response = await sendRequest('/posts/1',testData,'DELET');
         expect(response.status).to.equal(400)
+    })
+
+    it('DELETE post invalid request', async()=>{
+        const response = await sendRequest('/posts/1',testData,'DELETE');
+        expect(response.status.data).to.not.equal(200)
+        
     })
 
 })
